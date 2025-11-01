e6f30e49a14d8309d9ae7b74b6c6d24ff7957ef7
komen <cjepkorir09@gmail.com>
2025-11-01 18:31:01 +0300

    final commit

--- lib/app.py
+++ lib/app.py
@@ -6,4 +6,8 @@
 # print ("Hello sun!", end=" !!")
 # print ("Hello sky!", end="!!!\n")
 
+#print("Hello world!",end="")
+#print("Hello sun!",end="!!")
+#print("Hello sky!",end="!!!\n")
 print("Hello World! Pass this test, please.")
+
